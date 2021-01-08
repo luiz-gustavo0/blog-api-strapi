8d38b67b74eec5aea827dfeb787b70b6bd50002a
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-01-08 14:19:08 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b438c19fb6f8c026022a9172b273de3e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74f73f32-085d-4ebc-b4e0-49d97b13f8e1'
+};
